c590a14f670fe0f672804db642e24a16d68df093
bdebruijn <bdebruijn@luminize.nl>
2015-02-19 03:23:20 +0100

    delayline.c: add delay pin and logic for changing delay time

--- src/hal/components/delayline.c
+++ src/hal/components/delayline.c
@@ -19,46 +19,48 @@ MODULE_LICENSE("GPLv2");
 #define MAX_INST 8
 #define DEFAULT_SAMPLES 3
 #define MAX_SAMPLES 9       // a pose has 9 doubles
-#define DEFAULT_DELAY 1000  // 1 sec at default servo rate
+#define MAX_DELAY 1000      // 1 sec at default servo rate
 #define RB_HEADROOM 1.2     // ringbuffer size overallocation
 
 static char *names[MAX_INST] = { "delayline.0", };
 RTAPI_MP_ARRAY_STRING(names, MAX_INST,"delayline names");
 
-static int delay[MAX_INST] = {
-    DEFAULT_DELAY,DEFAULT_DELAY,DEFAULT_DELAY,DEFAULT_DELAY,
-    DEFAULT_DELAY,DEFAULT_DELAY,DEFAULT_DELAY,DEFAULT_DELAY
+static int max_delay[MAX_INST] = {
+	MAX_DELAY,MAX_DELAY,MAX_DELAY,MAX_DELAY,
+	MAX_DELAY,MAX_DELAY,MAX_DELAY,MAX_DELAY
 };
-RTAPI_MP_ARRAY_INT(delay, MAX_INST,"number of samples to delay for each line");
+RTAPI_MP_ARRAY_INT(max_delay, MAX_INST,"max number of samples to delay for each line");
 
 static int samples[MAX_INST] = {
-    DEFAULT_SAMPLES,DEFAULT_SAMPLES,DEFAULT_SAMPLES,DEFAULT_SAMPLES,
-    DEFAULT_SAMPLES,DEFAULT_SAMPLES,DEFAULT_SAMPLES,DEFAULT_SAMPLES,
+	DEFAULT_SAMPLES,DEFAULT_SAMPLES,DEFAULT_SAMPLES,DEFAULT_SAMPLES,
+	DEFAULT_SAMPLES,DEFAULT_SAMPLES,DEFAULT_SAMPLES,DEFAULT_SAMPLES,
 };
 RTAPI_MP_ARRAY_INT(samples, MAX_INST,"number of pins to sample in each line");
 
 typedef struct {
-    // pins
-    hal_bit_t *enable;           /* pin: enable this                   */
-    hal_bit_t *abort;            /* pin: abort pin                     */
-    hal_float_t *flt_in[MAX_SAMPLES];   /* pin: array incoming value          */
-    hal_float_t *flt_out[MAX_SAMPLES];  /* pin: array delayed value           */
-    hal_u32_t  *write_fail;      // error counter, write side
-    hal_u32_t  *read_fail;       // error counter, read side
-    hal_u32_t  *too_old;         // error counter, samples skipped because too old
+	// pins
+	hal_bit_t *enable;                  /* pin: enable this                   */
+	hal_bit_t *abort;                   /* pin: abort pin                     */
+	hal_float_t *flt_in[MAX_SAMPLES];   /* pin: array incoming value          */
+	hal_float_t *flt_out[MAX_SAMPLES];  /* pin: array delayed value           */
+	hal_u32_t	*pin_delay;             /* pin: delay time, standard zero     */
+	hal_u32_t  *write_fail;             // error counter, write side
+	hal_u32_t  *read_fail;              // error counter, read side
+	hal_u32_t  *too_old;                // error counter, samples skipped because too old
 
-    // other params & instance data
-    uint64_t  output_ts, input_ts;
-    unsigned  delay;             // delay in periods
-    int nsamples;                // number of pins in this line
-    size_t sample_size;          // sizeof(sample_t)  + num_channel * sizeof (hal_float_t)
-    hal_bit_t last_abort;        // tracking value for edge detection
-    char name[HAL_NAME_LEN + 1]; // of this instance
+	// other params & instance data
+	uint64_t  output_ts, input_ts;
+	unsigned  max_delay;         // max delay of this line in periods
+	unsigned  delay;             // delay in periods
+	int nsamples;                // number of pins in this line
+	size_t sample_size;          // sizeof(sample_t)  + num_channel * sizeof (hal_float_t)
+	hal_bit_t last_abort;        // tracking value for edge detection
+	char name[HAL_NAME_LEN + 1]; // of this instance
 } hal_delayline_t;
 
 typedef struct {
-    uint64_t timestamp;          /* timestamp of measurement           */
-    hal_float_t value[0];        /* measured value                     */
+	uint64_t timestamp;          /* timestamp of measurement           */
+	hal_float_t value[0];        /* measured value                     */
 } sample_t;
 
 
@@ -77,51 +79,51 @@ static int export_delayline(int n);
 // initialisation routine
 int rtapi_app_main(void)
 {
-    int n, retval;
+	int n, retval;
 
-    // determine number of instances
-    for (count = 0; (names[count] != NULL) && (count < MAX_INST); count++);
+	// determine number of instances
+	for (count = 0; (names[count] != NULL) && (count < MAX_INST); count++);
 
-    comp_id = hal_init(cname);
-    if (comp_id < 0) {
+	comp_id = hal_init(cname);
+	if (comp_id < 0) {
 	rtapi_print_msg(RTAPI_MSG_ERR,
 			"%s: ERROR: hal_init() failed, rc=%d\n", cname, comp_id);
 	return -1;
-    }
-
-    // export variables and function for each DELAYLINE loop */
-    for (n = 0; n < count; n++) {
-	if ((retval = export_delayline(n))) {
-	    rtapi_print_msg(RTAPI_MSG_ERR,
-			    "%s: ERROR: loop %d var export failed\n", cname, n);
-	    hal_exit(comp_id);
-	    return -1;
 	}
-    }
 
-    rtapi_print_msg(RTAPI_MSG_INFO,
-		    "%s: installed %d lines\n", cname, count);
-    hal_ready(comp_id);
-    return 0;
+	// export variables and function for each DELAYLINE loop */
+	for (n = 0; n < count; n++) {
+	if ((retval = export_delayline(n))) {
+		rtapi_print_msg(RTAPI_MSG_ERR,
+				"%s: ERROR: loop %d var export failed\n", cname, n);
+		hal_exit(comp_id);
+		return -1;
+	}
+	}
+
+	rtapi_print_msg(RTAPI_MSG_INFO,
+			"%s: installed %d lines\n", cname, count);
+	hal_ready(comp_id);
+	return 0;
 }
 
 // exiting routine
 void rtapi_app_exit(void)
 {
-    hal_exit(comp_id);
+	hal_exit(comp_id);
 
-    //detach from all rings, and delete them
-    int n;
-    for (n = 0; n < count; n++) {
+	//detach from all rings, and delete them
+	int n;
+	for (n = 0; n < count; n++) {
 	if (instance[n]) {
-	    hal_delayline_t *hd = instance[n]->scratchpad;
+		hal_delayline_t *hd = instance[n]->scratchpad;
 
-	    char ringname[HAL_NAME_LEN + 1];
-	    snprintf(ringname, HAL_NAME_LEN, "%s.samples", hd->name);
-	    hal_ring_detach(ringname, instance[n]);
-	    hal_ring_delete(ringname);
+		char ringname[HAL_NAME_LEN + 1];
+		snprintf(ringname, HAL_NAME_LEN, "%s.samples", hd->name);
+		hal_ring_detach(ringname, instance[n]);
+		hal_ring_delete(ringname);
 	}
-    }
+	}
 }
 
 
@@ -130,161 +132,200 @@ void rtapi_app_exit(void)
 */
 static void write_sample_to_ring(void *arg, long period)
 {
-    int j;
-    ringbuffer_t *rb = (ringbuffer_t *) arg;
-    hal_delayline_t *hd = rb->scratchpad;    // per-instance HAL data
-    sample_t *s;
+	int j;
+	ringbuffer_t *rb = (ringbuffer_t *) arg;
+	hal_delayline_t *hd = rb->scratchpad;    // per-instance HAL data
+	sample_t *s;
 
-    if (!*(hd->enable)) {	// skip if not sampling, just put the input 
-				//values into the output values
+	// if pin_delay > max_delay then use max_delay, otherwise
+	// use delay pin value
+
+	// if time has increased, then take action by setting the input_ts to the
+	// result of output_ts + delay. Otherwise do nothing. The situation of
+	// decreasing the time will be acted upon in read_sample_to_ring()
+	if (hd->pin_delay > (hal_u32_t * )((hd->input_ts) - (hd->output_ts))) {
+		if ( *(hd->pin_delay) > hd->max_delay) {
+			hd->delay = hd->max_delay;
+		}
+		else {
+			hd->delay = *(hd->pin_delay);
+		}
+			// set the new timestamp 
+			hd->input_ts = hd->output_ts + (uint64_t)(hd->delay);
+	}
+
+	if (!*(hd->enable)) {	// skip if not sampling, just put the input 
+							// values into the output values
 	for (j = 0; j < hd->nsamples; j++) {
 		*(hd->flt_out[j]) = *(hd->flt_in[j]);
-    	}
+		}
 	goto DONE;
-    }
+	}
 
-    // use non-copying write:
-    // allocate space in the ringbuffer and retrieve a pointer to it
-    if (record_write_begin(rb, (void ** )&s, hd->sample_size)) {
+	// use non-copying write:
+	// allocate space in the ringbuffer and retrieve a pointer to it
+	if (record_write_begin(rb, (void ** )&s, hd->sample_size)) {
 	*(hd->write_fail) += 1;
 	goto DONE;
-    }
+	}
 
-    // deposit record directly in rb memory
-    s->timestamp = hd->input_ts;
-    for (j = 0; j < hd->nsamples; j++) {
+	// deposit record directly in rb memory
+	s->timestamp = hd->input_ts;
+	for (j = 0; j < hd->nsamples; j++) {
 	s->value[j] = *(hd->flt_in[j]);
-    }
+	}
 
-    // commit the write given the actual write size (which is the same as given in
-    // record_write_begin in our case).
-    // this makes the record actually visible on the read side (advances pointer)
-    if (record_write_end(rb, s, hd->sample_size))
+	// commit the write given the actual write size (which is the same as given in
+	// record_write_begin in our case).
+	// this makes the record actually visible on the read side (advances pointer)
+	if (record_write_end(rb, s, hd->sample_size))
 	*(hd->write_fail) += 1;
 
  DONE:
-    hd->input_ts++;
+	hd->input_ts++;
 }
 
 // sample the pins to the current rb record
 static inline void apply(const sample_t *s, const hal_delayline_t *hd)
 {
-    int i;
-    for (i = 0; i < hd->nsamples; i++)
+	int i;
+	for (i = 0; i < hd->nsamples; i++)
 	*(hd->flt_out[i]) = s->value[i];
 }
 
 static void read_sample_from_ring(void *arg, long period)
 {
-    ringbuffer_t *rb = (ringbuffer_t *) arg;
-    hal_delayline_t *hd = rb->scratchpad;
-    const sample_t *s;
-    size_t size;
+	ringbuffer_t *rb = (ringbuffer_t *) arg;
+	hal_delayline_t *hd = rb->scratchpad;
+	const sample_t *s;
+	size_t size;
 
-    // detect rising edge on abort pin, and flush rb if so
-    if (*(hd->abort) && (*(hd->abort) ^ hd->last_abort)) {
+	// detect rising edge on abort pin, and flush rb if so
+	if (*(hd->abort) && (*(hd->abort) ^ hd->last_abort)) {
 	int dropped = record_flush(rb);
 	rtapi_print_msg(RTAPI_MSG_INFO,
 			"%s: %s aborted - dropped %d samples\n",
 			cname, hd->name, dropped);
-    }
+	}
 
-    // peek at the head of the queue
-    while (record_read(rb, (const void **)&s, &size) == 0) {
+	// if pin_delay < 0 then use 0, otherwise
+	// use delay pin value
 
-	// do nothing if timestamp is in the future
-	if (s->timestamp > hd->output_ts)
-	    goto NOTYET;
+	// if time has decreased, then take action by setting the output_ts to the
+	// result of input_ts - delay. Otherwise do nothing. This will result in
+	// loss of some records. Make sure you're in a safe situation! 
+	// The situation of increasing the time will be acted upon in
+	// write_sample_to_ring()
+	if (hd->pin_delay < (hal_u32_t * )((hd->input_ts - hd->output_ts))) {
+		if (hd->pin_delay < 0) {
+			hd->delay = 0;
+		}
+		else {
+			hd->delay = *(hd->pin_delay);
+		}
+			// set the new timestamp 
+			hd->input_ts =  hd->input_ts - (uint64_t)(hd->delay);
+	}
 
-	if (s->timestamp == hd->output_ts)
-	    // the time is right
-	    apply(s, hd);
-	else
-	    // skip old samples and bump an error counter
-	    *(hd->too_old) += 1;
+	// peek at the head of the queue
+	while (record_read(rb, (const void **)&s, &size) == 0) {
 
-	// sanity: if (size != hd->sample_size).. terribly wrong.
-	record_shift(rb); // consume record
-    }
+		// do nothing if timestamp is in the future
+		if (s->timestamp > hd->output_ts)
+			goto NOTYET;
+
+		if (s->timestamp == hd->output_ts)
+			// the time is right
+			apply(s, hd);
+		else
+			// skip old samples and bump an error counter
+			*(hd->too_old) += 1;
+
+		// sanity: if (size != hd->sample_size).. terribly wrong.
+		record_shift(rb); // consume record
+	}
  NOTYET:
-    hd->output_ts++; // always bump the timestamp
-    hd->last_abort = *(hd->abort);
+	hd->output_ts++; // always bump the timestamp
+	hd->last_abort = *(hd->abort);
 }
 
 static int export_delayline(int n)
 {
-    int retval, i;
-    char buf[HAL_NAME_LEN + 1];
+	int retval, i;
+	char buf[HAL_NAME_LEN + 1];
 
-    // determine the required size of the ringbuffer
-    size_t nsamples =  samples[n];
-    size_t sample_size = sizeof(sample_t) + (nsamples * sizeof(hal_float_t));
+	// determine the required size of the ringbuffer
+	size_t nsamples =  samples[n];
+	size_t sample_size = sizeof(sample_t) + (nsamples * sizeof(hal_float_t));
 
-    // add some headroom to be sure we dont overrun
-    size_t rbsize = record_space(sample_size) * delay[n] * RB_HEADROOM;
+	// add some headroom to be sure we dont overrun
+	size_t rbsize = record_space(sample_size) * max_delay[n] * RB_HEADROOM;
 
-    // create the delay queue
-    snprintf(buf, HAL_NAME_LEN, "%s.samples", names[n]);
-    if ((retval = hal_ring_new(buf, rbsize,
-			       sizeof(hal_delayline_t), ALLOC_HALMEM))) {
+	// create the delay queue
+	snprintf(buf, HAL_NAME_LEN, "%s.samples", names[n]);
+	if ((retval = hal_ring_new(buf, rbsize,
+				   sizeof(hal_delayline_t), ALLOC_HALMEM))) {
 	rtapi_print_msg(RTAPI_MSG_ERR,
 			"%s: failed to create new ring %s: %d\n",
 			cname, buf, retval);
 	return -1;
-    }
+	}
 
-    // use the per-using component ring access structure as the instance data,
-    // which will also give us a handle on the scratchpad which we use for
-    // HAL pins and other shared data
-    if ((instance[n] = hal_malloc(sizeof(ringbuffer_t))) == NULL)
+	// use the per-using component ring access structure as the instance data,
+	// which will also give us a handle on the scratchpad which we use for
+	// HAL pins and other shared data
+	if ((instance[n] = hal_malloc(sizeof(ringbuffer_t))) == NULL)
 	return -1;
-    if ((retval = hal_ring_attach(buf, instance[n], NULL))) {
+	if ((retval = hal_ring_attach(buf, instance[n], NULL))) {
 	rtapi_print_msg(RTAPI_MSG_ERR,
 			"%s: attach to ring %s failed: %d\n",
 			cname, buf, retval);
 	return -1;
-    }
+	}
 
-    // fill in instance data
-    hal_delayline_t *hd = instance[n]->scratchpad;
-    strncpy(hd->name, names[n], sizeof(hd->name));
-    hd->nsamples = nsamples;
-    hd->sample_size = sample_size;
-    hd->delay = delay[n];
+	// fill in instance data
+	hal_delayline_t *hd = instance[n]->scratchpad;
+	strncpy(hd->name, names[n], sizeof(hd->name));
+	hd->nsamples = nsamples;
+	hd->sample_size = sample_size;
+	hd->max_delay = max_delay[n];
+	// set delay standard to value of zero
+	hd->delay = 0;
+	hd->output_ts = 0;
+	hd->input_ts = hd->delay;
 
-    hd->output_ts = 0;
-    hd->input_ts =  hd->delay;
-
-    // init pins
-    for (i = 0; i < hd->nsamples; i++) {
+	// init pins
+	for (i = 0; i < hd->nsamples; i++) {
 	if (((retval = hal_pin_float_newf(HAL_IN, &(hd->flt_in[i]), comp_id,
 					  "%s.in%d", hd->name, i))) ||
-	    ((retval = hal_pin_float_newf(HAL_OUT, &(hd->flt_out[i]), comp_id,
+		((retval = hal_pin_float_newf(HAL_OUT, &(hd->flt_out[i]), comp_id,
 					  "%s.out%d", hd->name, i))))
-	    return retval;
-    }
-    if (((retval = hal_pin_bit_newf(HAL_IN, &(hd->enable), comp_id,
-				    "%s.enable",  hd->name))) ||
+		return retval;
+	}
+	if (((retval = hal_pin_bit_newf(HAL_IN, &(hd->enable), comp_id,
+					"%s.enable",  hd->name))) ||
 	((retval = hal_pin_bit_newf(HAL_IN, &(hd->abort), comp_id,
-				    "%s.abort",  hd->name))) ||
+					"%s.abort",  hd->name))) ||
+	((retval = hal_pin_u32_newf(HAL_IN, &(hd->pin_delay), comp_id,
+					  "%s.delay", hd->name))) ||
 	((retval = hal_pin_u32_newf(HAL_OUT, &(hd->write_fail), comp_id,
-				    "%s.write-fail", hd->name))) ||
+					"%s.write-fail", hd->name))) ||
 	((retval = hal_pin_u32_newf(HAL_OUT, &(hd->read_fail), comp_id,
-				    "%s.too-old", hd->name))) ||
+					"%s.too-old", hd->name))) ||
 	((retval = hal_pin_u32_newf(HAL_OUT, &(hd->too_old), comp_id,
-				    "%s.read-fail", hd->name))))
+					"%s.read-fail", hd->name))))
 	return retval;
 
-    // export thread functions
-    rtapi_snprintf(buf, sizeof(buf), "%s.sample", hd->name);
-    if ((retval = hal_export_funct(buf, write_sample_to_ring,
+	// export thread functions
+	rtapi_snprintf(buf, sizeof(buf), "%s.sample", hd->name);
+	if ((retval = hal_export_funct(buf, write_sample_to_ring,
 				   instance[n], 1, 0, comp_id))) {
 	return retval;
-    }
-    rtapi_snprintf(buf, sizeof(buf), "%s.output", hd->name);
-    if ((retval = hal_export_funct(buf, read_sample_from_ring,
+	}
+	rtapi_snprintf(buf, sizeof(buf), "%s.output", hd->name);
+	if ((retval = hal_export_funct(buf, read_sample_from_ring,
 				   instance[n], 1, 0, comp_id))) {
 	return retval;
-    }
-    return 0;
+	}
+	return 0;
 }
