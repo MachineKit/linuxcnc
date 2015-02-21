43e1c1fc0ca3238c00ce753d486cb6e46363db2b
bdebruijn <bdebruijn@luminize.nl>
2015-02-21 11:35:26 +0100

    delayline.c: add different pintypes for delayline + hal example

--- src/hal/components/delayline.c
+++ src/hal/components/delayline.c
@@ -1,7 +1,15 @@
-/*
-  component written by Bas de Bruijn
-  todo other info and stuff
-*/
+// this component brings a multi-purpose delay line.
+//
+// input pins of bit, float, u32 or s32 will be measured and put in a buffer
+// with a timestamp (hal_delayline_t->input_ts).
+// The reading part of this component will look at the record in
+// the buffer, and when the timestamp matches it's own timestamp
+// (hal_delayline_t->output_ts which is less then hal_delayline_t->input_ts)
+// the value will be put into the output pins.
+//
+// changing the hal_delayline_t->delay pin will set the amount of delay
+//
+// see manpage delayline.9
 
 #ifndef MODULE
 #include <stdint.h>
@@ -17,7 +25,7 @@ MODULE_DESCRIPTION("Delay line component for Machinekit HAL");
 MODULE_LICENSE("GPLv2");
 
 #define MAX_INST 8
-#define DEFAULT_SAMPLES 3
+#define DEFAULT_SAMPLES 1
 #define MAX_SAMPLES 9       // a pose has 9 doubles
 #define MAX_DELAY 1000      // 1 sec at default servo rate
 #define RB_HEADROOM 1.2     // ringbuffer size overallocation
@@ -29,38 +37,44 @@ static int max_delay[MAX_INST] = {
 	MAX_DELAY,MAX_DELAY,MAX_DELAY,MAX_DELAY,
 	MAX_DELAY,MAX_DELAY,MAX_DELAY,MAX_DELAY
 };
-RTAPI_MP_ARRAY_INT(max_delay, MAX_INST,"max number of samples to delay for each line");
+RTAPI_MP_ARRAY_INT(max_delay, MAX_INST,"delayline max number of samples");
 
-static int samples[MAX_INST] = {
-	DEFAULT_SAMPLES,DEFAULT_SAMPLES,DEFAULT_SAMPLES,DEFAULT_SAMPLES,
-	DEFAULT_SAMPLES,DEFAULT_SAMPLES,DEFAULT_SAMPLES,DEFAULT_SAMPLES,
-};
-RTAPI_MP_ARRAY_INT(samples, MAX_INST,"number of pins to sample in each line");
+
+char *samples[MAX_INST];
+RTAPI_MP_ARRAY_STRING(samples, MAX_INST, "delayline pintype sample specifiers");
+
+typedef union {
+	// union for using channels of different types
+	hal_float_t		*pin_flt;
+	hal_bit_t		*pin_bit;
+	hal_u32_t		*pin_u32;
+	hal_s32_t		*pin_s32;
+} u_value;
 
 typedef struct {
 	// pins
-	hal_bit_t *enable;                  /* pin: enable this                   */
-	hal_bit_t *abort;                   /* pin: abort pin                     */
-	hal_float_t *flt_in[MAX_SAMPLES];   /* pin: array incoming value          */
-	hal_float_t *flt_out[MAX_SAMPLES];  /* pin: array delayed value           */
-	hal_u32_t	*pin_delay;             /* pin: delay time, standard zero     */
-	hal_u32_t  *write_fail;             // error counter, write side
-	hal_u32_t  *read_fail;              // error counter, read side
-	hal_u32_t  *too_old;                // error counter, samples skipped because too old
+	hal_bit_t		*enable;					// pin: enable this
+	hal_bit_t		*abort;						// pin: abort pin
+	u_value			*pins_in[MAX_SAMPLES];		// pin: array incoming value
+	u_value			*pins_out[MAX_SAMPLES];		// pin: array delayed value
+	hal_u32_t		*pin_delay;					// pin: delay time, standard zero
+	hal_u32_t 		*write_fail;				// error counter, write side
+	hal_u32_t 		*read_fail;					// error counter, read side
+	hal_u32_t 		*too_old;					// error counter, samples skipped because too old
 
 	// other params & instance data
 	uint64_t  output_ts, input_ts;
-	unsigned  max_delay;         // max delay of this line in periods
-	unsigned  delay;             // delay in periods
-	int nsamples;                // number of pins in this line
-	size_t sample_size;          // sizeof(sample_t)  + num_channel * sizeof (hal_float_t)
-	hal_bit_t last_abort;        // tracking value for edge detection
-	char name[HAL_NAME_LEN + 1]; // of this instance
+	unsigned  max_delay;         	// max delay of this line in periods
+	unsigned  delay;             	// delay in periods
+	int nsamples;                	// number of pins in this line
+	size_t sample_size;          	// size determined by union u_value
+	hal_bit_t last_abort;        	// tracking value for edge detection
+	char name[HAL_NAME_LEN + 1];	// of this instance
 } hal_delayline_t;
 
 typedef struct {
-	uint64_t timestamp;          /* timestamp of measurement           */
-	hal_float_t value[0];        /* measured value                     */
+	uint64_t timestamp;			// timestamp of measurement
+	u_value value[0];			// measured value
 } sample_t;
 
 
@@ -75,6 +89,7 @@ static void read_sample_from_ring(void *arg, long period);
 
 // this is the routine where the pins are made
 static int export_delayline(int n);
+static int return_instance_samples(int n);
 
 // initialisation routine
 int rtapi_app_main(void)
@@ -90,15 +105,49 @@ int rtapi_app_main(void)
 			"%s: ERROR: hal_init() failed, rc=%d\n", cname, comp_id);
 	return -1;
 	}
-
-	// export variables and function for each DELAYLINE loop */
-	for (n = 0; n < count; n++) {
-	if ((retval = export_delayline(n))) {
-		rtapi_print_msg(RTAPI_MSG_ERR,
-				"%s: ERROR: loop %d var export failed\n", cname, n);
-		hal_exit(comp_id);
-		return -1;
+	
+	// check if nr of array members in sample lines configuration string
+	// correspond with number of instances. initiation is like:
+	// samples="bb,sf,fus" (bit, float, signed, unsigned)
+	if (!samples[0]) {
+        rtapi_print_msg(RTAPI_MSG_ERR,
+        	"%s : ERROR: a string declaring valid pintypes is needed\n",
+        	cname);
+        hal_exit(comp_id);
+        return -1;
+    }
+    // parse the samples array for sanity
+    int nr_of_samples;
+	for (n = 0; (samples[n] != NULL) || (n < count); n++) {	
+		nr_of_samples = return_instance_samples(n);
+		if (nr_of_samples <= 0) {
+			rtapi_print_msg(RTAPI_MSG_ERR,
+				"%s: ERROR: erroneous number of samples (%d) for instance %d\n",
+				cname, nr_of_samples, n);
+			hal_exit(comp_id);
+			return -1;
+		}
+		rtapi_print_msg(RTAPI_MSG_INFO,
+			"%s: there are %d samples for instance %d\n",
+			cname, nr_of_samples, n);
 	}
+	if (n != count) {
+		rtapi_print_msg(RTAPI_MSG_ERR,
+				"%s: ERROR: \"samples=\" array amount mismatch with number of"
+				" instances\n",	cname);
+			hal_exit(comp_id);
+			return -1;
+	}
+
+	// done with input checking, and all should be fine
+	// export variables and function for each DELAYLINE loop
+	for (n = 0; n < count; n++) {
+		if ((retval = export_delayline(n))) {
+			rtapi_print_msg(RTAPI_MSG_ERR,
+					"%s: ERROR: loop %d var export failed\n", cname, n);
+			hal_exit(comp_id);
+			return -1;
+		}
 	}
 
 	rtapi_print_msg(RTAPI_MSG_INFO,
@@ -115,21 +164,20 @@ void rtapi_app_exit(void)
 	//detach from all rings, and delete them
 	int n;
 	for (n = 0; n < count; n++) {
-	if (instance[n]) {
-		hal_delayline_t *hd = instance[n]->scratchpad;
+		if (instance[n]) {
+			hal_delayline_t *hd = instance[n]->scratchpad;
 
-		char ringname[HAL_NAME_LEN + 1];
-		snprintf(ringname, HAL_NAME_LEN, "%s.samples", hd->name);
-		hal_ring_detach(ringname, instance[n]);
-		hal_ring_delete(ringname);
-	}
+			char ringname[HAL_NAME_LEN + 1];
+			snprintf(ringname, HAL_NAME_LEN, "%s.samples", hd->name);
+			hal_ring_detach(ringname, instance[n]);
+			hal_ring_delete(ringname);
+		}
 	}
 }
 
 
-/* write_sample_to_ring() will take the pins with the actual value and
-   put them into the ring.
-*/
+// write_sample_to_ring() will take the pins with the actual value 
+// and put them into the ring.
 static void write_sample_to_ring(void *arg, long period)
 {
 	int j;
@@ -157,7 +205,7 @@ static void write_sample_to_ring(void *arg, long period)
 	if (!*(hd->enable)) {	// skip if not sampling, just put the input 
 							// values into the output values
 	for (j = 0; j < hd->nsamples; j++) {
-		*(hd->flt_out[j]) = *(hd->flt_in[j]);
+		*(hd->pins_out[j]) = *(hd->pins_in[j]);
 		}
 	goto DONE;
 	}
@@ -172,12 +220,12 @@ static void write_sample_to_ring(void *arg, long period)
 	// deposit record directly in rb memory
 	s->timestamp = hd->input_ts;
 	for (j = 0; j < hd->nsamples; j++) {
-	s->value[j] = *(hd->flt_in[j]);
+	s->value[j] = *(hd->pins_in[j]);
 	}
 
-	// commit the write given the actual write size (which is the same as given in
-	// record_write_begin in our case).
-	// this makes the record actually visible on the read side (advances pointer)
+	// commit the write given the actual write size (which is the same
+	// as given in record_write_begin in our case). This makes the
+	// record actually visible on the read side (advances pointer)
 	if (record_write_end(rb, s, hd->sample_size))
 	*(hd->write_fail) += 1;
 
@@ -190,7 +238,7 @@ static inline void apply(const sample_t *s, const hal_delayline_t *hd)
 {
 	int i;
 	for (i = 0; i < hd->nsamples; i++)
-	*(hd->flt_out[i]) = s->value[i];
+	*(hd->pins_out[i]) = s->value[i];
 }
 
 static void read_sample_from_ring(void *arg, long period)
@@ -249,14 +297,41 @@ static void read_sample_from_ring(void *arg, long period)
 	hd->last_abort = *(hd->abort);
 }
 
+static int return_instance_samples(int n)
+{
+	int i, nr_of_samples = 0;
+	char character;
+	// traverse the string to count the number of correct pins
+	for (i = 0; (character = samples[n][i]); i++) {
+		switch (character) {
+			case 'b':
+			case 'B':
+			case 'f':
+			case 'F':
+			case 's':
+			case 'S':
+			case 'u':
+			case 'U':
+				nr_of_samples++;
+				break;
+			default:
+				rtapi_print_msg(RTAPI_MSG_ERR, "invalid character in "
+					"\"samples=\" string. Needs to be only b,f,s or u\n");
+				hal_exit(comp_id);
+				return -1;
+		}
+	}
+	return nr_of_samples;	
+}
+
 static int export_delayline(int n)
 {
-	int retval, i;
+	int retval, nr_of_samples, i;
 	char buf[HAL_NAME_LEN + 1];
 
 	// determine the required size of the ringbuffer
-	size_t nsamples =  samples[n];
-	size_t sample_size = sizeof(sample_t) + (nsamples * sizeof(hal_float_t));
+	nr_of_samples = return_instance_samples(n);
+	size_t sample_size = sizeof(sample_t) + (nr_of_samples * sizeof(u_value));
 
 	// add some headroom to be sure we dont overrun
 	size_t rbsize = record_space(sample_size) * max_delay[n] * RB_HEADROOM;
@@ -286,7 +361,7 @@ static int export_delayline(int n)
 	// fill in instance data
 	hal_delayline_t *hd = instance[n]->scratchpad;
 	strncpy(hd->name, names[n], sizeof(hd->name));
-	hd->nsamples = nsamples;
+	hd->nsamples = nr_of_samples;
 	hd->sample_size = sample_size;
 	hd->max_delay = max_delay[n];
 	// set delay standard to value of zero
@@ -295,13 +370,105 @@ static int export_delayline(int n)
 	hd->input_ts = hd->delay;
 
 	// init pins
+	char character;
 	for (i = 0; i < hd->nsamples; i++) {
-	if (((retval = hal_pin_float_newf(HAL_IN, &(hd->flt_in[i]), comp_id,
-					  "%s.in%d", hd->name, i))) ||
-		((retval = hal_pin_float_newf(HAL_OUT, &(hd->flt_out[i]), comp_id,
-					  "%s.out%d", hd->name, i))))
-		return retval;
+		character = samples[n][i];
+		rtapi_print_msg(RTAPI_MSG_INFO, "\"samples=\" string = %s"
+										" character %d = %c \n", 
+										samples[n], i, character);
+		switch (character) {
+			case 'b':
+			case 'B':
+				// create bit sample pins
+				rtapi_snprintf(buf, sizeof(buf), "%s.bit-in%d", hd->name, i);
+				retval = hal_pin_new(buf, HAL_BIT, HAL_IN, 
+					&hd->pins_in[i], comp_id );
+				if (retval != 0) {
+					rtapi_print_msg(RTAPI_MSG_ERR,
+						"%s: ERROR: creation of bit-in%d for instance %d\n",
+						cname, i, n);
+					return retval;
+				}
+				rtapi_snprintf(buf, sizeof(buf), "%s.bit-out%d", hd->name, i);
+				retval = hal_pin_new(buf, HAL_BIT, HAL_OUT, 
+					&hd->pins_out[i], comp_id );
+				if (retval != 0) {
+					rtapi_print_msg(RTAPI_MSG_ERR,
+						"%s: ERROR: creation of bit-out%d for instance %d\n",
+						cname, i, n);
+					return retval;
+				}
+				break;
+			case 'f':
+			case 'F':
+				// create float sample pins
+				rtapi_snprintf(buf, sizeof(buf), "%s.flt-in%d", hd->name, i);
+				retval = hal_pin_new(buf, HAL_FLOAT, HAL_IN, 
+					&hd->pins_in[i], comp_id );
+				if (retval != 0) {
+					rtapi_print_msg(RTAPI_MSG_ERR,
+						"%s: ERROR: creation of flt-in%d for instance %d\n",
+						cname, i, n);
+					return retval;
+				}
+				rtapi_snprintf(buf, sizeof(buf), "%s.flt-out%d", hd->name, i);
+				retval = hal_pin_new(buf, HAL_FLOAT, HAL_OUT, 
+					&hd->pins_out[i], comp_id );
+				if (retval != 0) {
+					rtapi_print_msg(RTAPI_MSG_ERR,
+						"%s: ERROR: creation of flt-out%d for instance %d\n",
+						cname, i, n);
+					return retval;
+				}
+				break;
+			case 's':
+			case 'S':
+				// create s32 sample pins
+				rtapi_snprintf(buf, sizeof(buf), "%s.s32-in%d", hd->name, i);
+				retval = hal_pin_new(buf, HAL_S32, HAL_IN, 
+					&hd->pins_in[i], comp_id );
+				if (retval != 0) {
+					rtapi_print_msg(RTAPI_MSG_ERR,
+						"%s: ERROR: creation of s32-in%d for instance %d\n",
+						cname, i, n);
+					return retval;
+				}
+				rtapi_snprintf(buf, sizeof(buf), "%s.s32-out%d", hd->name, i);
+				retval = hal_pin_new(buf, HAL_S32, HAL_OUT, 
+					&hd->pins_out[i], comp_id );
+				if (retval != 0) {
+					rtapi_print_msg(RTAPI_MSG_ERR,
+						"%s: ERROR: creation of s32-out%d for instance %d\n",
+						cname, i, n);
+					return retval;
+				}
+				break;
+			case 'u':
+			case 'U':
+				// create u32 sample pins
+				rtapi_snprintf(buf, sizeof(buf), "%s.u32-in%d", hd->name, i);
+				retval = hal_pin_new(buf, HAL_U32, HAL_IN, 
+					&hd->pins_in[i], comp_id );
+				if (retval != 0) {
+					rtapi_print_msg(RTAPI_MSG_ERR,
+						"%s: ERROR: creation of u32-in%d for instance %d\n",
+						cname, i, n);
+					return retval;
+				}
+				rtapi_snprintf(buf, sizeof(buf), "%s.u32-out%d", hd->name, i);
+				retval = hal_pin_new(buf, HAL_U32, HAL_OUT, 
+					&hd->pins_out[i], comp_id );
+				if (retval != 0) {
+					rtapi_print_msg(RTAPI_MSG_ERR,
+						"%s: ERROR: creation of u32-out%d for instance %d\n",
+						cname, i, n);
+					return retval;
+				}
+				break;
+			}
 	}
+
+	// create other pins
 	if (((retval = hal_pin_bit_newf(HAL_IN, &(hd->enable), comp_id,
 					"%s.enable",  hd->name))) ||
 	((retval = hal_pin_bit_newf(HAL_IN, &(hd->abort), comp_id,
