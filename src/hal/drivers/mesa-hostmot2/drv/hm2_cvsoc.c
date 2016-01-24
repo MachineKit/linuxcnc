9675e395685339566a4c3337c5de8666634cd758
Michael Brown <producer@holotronic.dk>
2016-01-24 06:16:19 +0100

    Do Not Merge (contains some pseudocode) 1 step forward....

--- src/hal/drivers/mesa-hostmot2/drv/hm2_cvsoc.c
+++ src/hal/drivers/mesa-hostmot2/drv/hm2_cvsoc.c
@@ -202,229 +202,63 @@ static int hm2_cvsoc_write(hm2_lowlevel_io_t *this, u32 addr, void *buffer, int 
 
 
 /*
-static int hm2_plx9030_program_fpga(hm2_lowlevel_io_t *this, const bitfile_t *bitfile) {
-    hm2_cvsoc_t *board = this->private;
-    int i;
-    u32 status, control;
+static int hm2_cvsoc_program_fpga(hm2_lowlevel_io_t *this, const bitfile_t *bitfile) {
 
-    // set /WRITE low for data transfer, and turn on LED
-    status = inl(board->ctrl_base_addr + CTRL_STAT_OFFSET);
-    control = status & ~_WRITE_MASK & ~_LED_MASK;
-    outl(control, board->ctrl_base_addr + CTRL_STAT_OFFSET);
-
-    // program the FPGA
-    for (i = 0; i < bitfile->e.size; i ++) {
-        outb(bitfile_reverse_bits(bitfile->e.data[i]), board->data_base_addr);
-    }
-
-    // all bytes transferred, make sure FPGA is all set up now
-    status = inl(board->ctrl_base_addr + CTRL_STAT_OFFSET);
-    if (!(status & _INIT_MASK)) {
-	// /INIT goes low on CRC error
-	THIS_ERR("FPGA asserted /INIT: CRC error\n");
-        goto fail;
-    }
-    if (!(status & DONE_MASK)) {
-	THIS_ERR("FPGA did not assert DONE\n");
-	goto fail;
-    }
-
-    // turn off write enable and LED
-    control = status | _WRITE_MASK | _LED_MASK;
-    outl(control, board->ctrl_base_addr + CTRL_STAT_OFFSET);
+//    disable bridges:  
+    echo 0 > hps2fpga, lwhps2fpga, fpga2hps
+    cat bitfile > /dev/fpga
+    //    enable bridges:  
+    echo 1 > hps2fpga, lwhps2fpga, fpga2hps
 
     return 0;
 
 
 fail:
-    // set /PROGRAM low (reset device), /WRITE high and LED off
-    status = inl(board->ctrl_base_addr + CTRL_STAT_OFFSET);
-    control = status & ~_PROGRAM_MASK;
-    control |= _WRITE_MASK | _LED_MASK;
-    outl(control, board->ctrl_base_addr + CTRL_STAT_OFFSET);
+    ???
     return -EIO;
-}
-
-
-static int hm2_plx9030_reset(hm2_lowlevel_io_t *this) {
-    hm2_cvsoc_t *board = this->private;
-    u32 status;
-    u32 control;
-
-    status = inl(board->ctrl_base_addr + CTRL_STAT_OFFSET);
-
-    // set /PROGRAM bit low to reset the FPGA
-    control = status & ~_PROGRAM_MASK;
-
-    // set /WRITE and /LED high (idle state)
-    control |= _WRITE_MASK | _LED_MASK;
-
-    // and write it back
-    outl(control, board->ctrl_base_addr + CTRL_STAT_OFFSET);
-
-    // verify that /INIT and DONE went low
-    status = inl(board->ctrl_base_addr + CTRL_STAT_OFFSET);
-    if (status & (DONE_MASK | _INIT_MASK)) {
-	THIS_ERR(
-            "FPGA did not reset: /INIT = %d, DONE = %d\n",
-	    (status & _INIT_MASK ? 1 : 0),
-            (status & DONE_MASK ? 1 : 0)
-        );
-	return -EIO;
-    }
-
-    // set /PROGRAM high, let FPGA come out of reset
-    control = status | _PROGRAM_MASK;
-    outl(control, board->ctrl_base_addr + CTRL_STAT_OFFSET);
-
-    // wait for /INIT to go high when it finishes clearing memory
-    // This should take no more than 100uS.  If we assume each PCI read
-    // takes 30nS (one PCI clock), that is 3300 reads.  Reads actually
-    // take several clocks, but even at a microsecond each, 3.3mS is not
-    // an excessive timeout value
-    {
-        int count = 3300;
-
-        do {
-            status = inl(board->ctrl_base_addr + CTRL_STAT_OFFSET);
-            if (status & _INIT_MASK) break;
-        } while (count-- > 0);
-
-        if (count == 0) {
-            THIS_ERR("FPGA did not come out of /INIT\n");
-            return -EIO;
-        }
-    }
-
-    return 0;
-}
-
-
-// fix up LASxBRD READY if needed
-static void hm2_plx9030_fixup_LASxBRD_READY(hm2_cvsoc_t *board) {
-    hm2_lowlevel_io_t *this = &board->llio;
-    int offsets[] = { LAS0BRD_OFFSET, LAS1BRD_OFFSET, LAS2BRD_OFFSET, LAS3BRD_OFFSET };
-    int i;
-
-    for (i = 0; i < 4; i ++) {
-        u32 val;
-        int addr = board->ctrl_base_addr + offsets[i];
-
-        val = inl(addr);
-        if (!(val & LASxBRD_READY)) {
-            THIS_INFO("LAS%dBRD #READY is off, enabling now\n", i);
-            val |= LASxBRD_READY;
-            outl(val, addr);
-        }
-    }
-}
-
-
-
-
-static int hm2_plx9054_program_fpga(hm2_lowlevel_io_t *this, const bitfile_t *bitfile) {
-    hm2_cvsoc_t *board = this->private;
-    int i;
-    u32 status;
-
-    // program the FPGA
-    for (i = 0; i < bitfile->e.size; i ++) {
-        outb(bitfile_reverse_bits(bitfile->e.data[i]), board->data_base_addr);
-    }
-
-    // all bytes transferred, make sure FPGA is all set up now
-    for (i = 0; i < DONE_WAIT_5I22; i++) {
-        status = inl(board->ctrl_base_addr + CTRL_STAT_OFFSET_5I22);
-        if (status & DONE_MASK_5I22) break;
-    }
-    if (i >= DONE_WAIT_5I22) {
-        THIS_ERR("Error: Not /DONE; programming not completed.\n");
-        return -EIO;
-    }
-
-    return 0;
-}
-
-
-static int hm2_plx9054_reset(hm2_lowlevel_io_t *this) {
-    hm2_cvsoc_t *board = this->private;
-    int i;
-    u32 status, control;
-
-    // set GPIO bits to GPIO function
-    status = inl(board->ctrl_base_addr + CTRL_STAT_OFFSET_5I22);
-    control = status | DONE_ENABLE_5I22 | _PROG_ENABLE_5I22;
-    outl(control, board->ctrl_base_addr + CTRL_STAT_OFFSET_5I22);
-
-    // Turn off /PROGRAM bit and insure that DONE isn't asserted
-    outl(control & ~_PROGRAM_MASK_5I22, board->ctrl_base_addr + CTRL_STAT_OFFSET_5I22);
-
-    status = inl(board->ctrl_base_addr + CTRL_STAT_OFFSET_5I22);
-    if (status & DONE_MASK_5I22) {
-        // Note that if we see DONE at the start of programming, it's most
-        // likely due to an attempt to access the FPGA at the wrong I/O
-        // location.
-        THIS_ERR("/DONE status bit indicates busy at start of programming\n");
-        return -EIO;
-    }
-
-    // turn on /PROGRAM output bit
-    outl(control | _PROGRAM_MASK_5I22, board->ctrl_base_addr + CTRL_STAT_OFFSET_5I22);
-
-    // Delay for at least 100 uS. to allow the FPGA to finish its reset
-    // sequencing.  3300 reads is at least 100 us, could be as long as a
-    // few ms
-    for (i = 0; i < 3300; i++) {
-        status = inl(board->ctrl_base_addr + CTRL_STAT_OFFSET);
-    }
-
-    return 0;
 }
 */
 
-
-
+/*
+static int hm2_cvsoc_reset(hm2_lowlevel_io_t *this) {
+    return 0;
+}
+*/
 // 
 // misc internal functions
 //
 
 
-static int hm2_cvsoc_probe(struct pci_dev *dev, const struct dts_device_id *id) {
+static int hm2_cvsoc_probe(struct cvsoc_dev *dev, const struct dts_device_id *id) {
     int r;
     hm2_cvsoc_t *board;
     hm2_lowlevel_io_t *this;
 
-
-    if (num_boards >= HM2_SOC_MAX_BOARDS) {
+// could there be more than 1 hm2 interface needed on a soc ?
+/*    if (num_boards >= HM2_SOC_MAX_BOARDS) {
         LL_PRINT("skipping hm2 soc interface at %s, this driver can only handle %d\n", dts_name(dev), HM2_SOC_MAX_BOARDS);
         return -EINVAL;
     }
-/*
-// NOTE: this enables the board's BARs -- this fixes the Arty bug
-    if (cvsoc_enable_device(dev)) {
-        LL_PRINT("skipping hm2 soc interface  at %s, failed to enable UIO device\n", dts_name(dev));
-        return failed_errno = -ENODEV;
-    }
-*/ // Dont think this is needed.
-
+*/
     board = &hm2_cvsoc_board[num_boards];
     this = &board->llio;
     memset(this, 0, sizeof(hm2_lowlevel_io_t));
 
 //    switch (dev->name) {
-    switch (dev->name) {
+    switch (dts_device_id->name) {
             
-        case hm2reg-io:
-        case HM2_PCI_SSDEV_6I25: {
-            if (dev->name == hm2reg-io) {
-                LL_PRINT("discovered 5i25 at %s\n", dts_name(dev));
-                rtapi_snprintf(board->llio.name, sizeof(board->llio.name), "hm2_5i25.%d", num_5i25);
+        case hm2-socfpga: {
+            if (dts_device_id->compatible->name == hm2reg-io) {
+//                LL_PRINT("discovered hm2reg-io at %s\n", dts_name(dev));
+                LL_PRINT("discovered hm2reg-io at entity (machkt,hm2reg-io-1.0.)\n");                
+//                rtapi_snprintf(board->llio.name, sizeof(board->llio.name), "hm2_5i25.%d", num_5i25);
                 num_5i25 ++;
-            } else {
-                LL_PRINT("discovered 6i25 at %s\n", dts_name(dev));
-                rtapi_snprintf(board->llio.name, sizeof(board->llio.name), "hm2_6i25.%d", num_6i25);
-                num_6i25 ++;
             }
+//            else {
+//                LL_PRINT("discovered 6i25 at %s\n", dts_name(dev));
+//                rtapi_snprintf(board->llio.name, sizeof(board->llio.name), "hm2_6i25.%d", num_6i25);
+//                num_6i25 ++;
+//            }
             board->llio.num_ioport_connectors = 2;
             board->llio.pins_per_connector = 17;
             board->llio.ioport_connector_name[0] = "P3";
@@ -435,15 +269,16 @@ static int hm2_cvsoc_probe(struct pci_dev *dev, const struct dts_device_id *id) 
         }
 
         default: {
-            LL_ERR("unknown subsystem device id 0x%04x\n", dev->name);
+//            LL_ERR("unknown subsystem device id 0x%04x\n", dev->name);
+            LL_ERR("unknown dts subsystem \n");
             return failed_errno = -ENODEV;
         }
     }
 
 
-    switch (dev->group) {
-        case HM2_PCI_DEV_MESA5I25:
-        case HM2_PCI_DEV_MESA6I25: {
+    switch (dev->name) {
+//        case HM2_PCI_DEV_MESA5I25:
+        case hm2-socfpga: {
               // mksocfpga_io_hm2 is 64K mem (32 bit)
 //            board->len = pci_resource_len(dev, 0);
             board->base = pci_ioremap_bar(dev, 0);
@@ -483,7 +318,7 @@ static int hm2_cvsoc_probe(struct pci_dev *dev, const struct dts_device_id *id) 
     }
 
 //    THIS_PRINT("initialized AnyIO board at %s\n", dts_name(dev));
-    THIS_PRINT("initialized AnyIO board at %s\n", cvsoc_name(dev));
+    THIS_PRINT("initialized AnyIO HM2 core at %s\n", cvsoc_name(dev));
     
     num_boards ++;
     return 0;
@@ -527,7 +362,7 @@ static void hm2_cvsoc_remove(struct pci_dev *dev) {
 }
 
 
-static struct pci_driver hm2_cvsoc_driver = {
+static struct uio_driver hm2_cvsoc_driver = {
 	.name = HM2_LLIO_NAME,
 	.id_table = hm2_cvsoc_tbl,
 	.probe = hm2_cvsoc_probe,
@@ -538,12 +373,12 @@ static struct pci_driver hm2_cvsoc_driver = {
 int rtapi_app_main(void) {
     int r = 0;
 
-    LL_PRINT("loading Mesa AnyIO HostMot2 driver version " HM2_SOC_VERSION "\n");
+    LL_PRINT("loading Mesa AnyIO HostMot2 soc driver version " HM2_SOC_VERSION "\n");
 
     comp_id = hal_init(HM2_LLIO_NAME);
     if (comp_id < 0) return comp_id;
 
-    r = pci_register_driver(&hm2_cvsoc_driver);
+    r = uio_register_driver(&hm2_cvsoc_driver);
     if (r != 0) {
         LL_ERR("error registering PCI driver\n");
         hal_exit(comp_id);
@@ -553,7 +388,7 @@ int rtapi_app_main(void) {
     if(failed_errno) {
 	// at least one card registration failed
 	hal_exit(comp_id);
-	pci_unregister_driver(&hm2_cvsoc_driver);
+	uio_unregister_driver(&hm2_cvsoc_driver);
 	return failed_errno;
     }
 
@@ -571,7 +406,7 @@ int rtapi_app_main(void) {
 
 
 void rtapi_app_exit(void) {
-    pci_unregister_driver(&hm2_cvsoc_driver);
+    uio_unregister_driver(&hm2_cvsoc_driver);
     LL_PRINT("driver unloaded\n");
     hal_exit(comp_id);
 }
